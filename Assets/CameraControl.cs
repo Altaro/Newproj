c7d45f926f626cad8db9d33677486c1ebf674de1
Dimitry <AltaroX@gmail.com>
2013-02-23 15:18:25 +0200

    0.0.0.3a
    Ещё раз изменил CameraControl, добавил туда скорость перемещения камеры
    переменной.

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
-	 private Transform myTransform;
+	private Transform myTransform;
+	public float cameraSpeed=40;
 	
 	void Awake(){
 		myTransform=transform;
@@ -17,16 +18,16 @@ public class CameraControl : MonoBehaviour {
 	
 	void Update () {
 	if(Input.GetKey(KeyCode.W)){
-		myTransform.position+= Vector3.forward*10*Time.deltaTime;
+		myTransform.position+= Vector3.forward*cameraSpeed*Time.deltaTime;
 		}	
 	if(Input.GetKey(KeyCode.S)){
-		myTransform.position-= Vector3.forward*10*Time.deltaTime;
+		myTransform.position-= Vector3.forward*cameraSpeed*Time.deltaTime;
 		}
 	if(Input.GetKey(KeyCode.A)){
-		myTransform.position-= Vector3.right*10*Time.deltaTime;
+		myTransform.position-= Vector3.right*cameraSpeed*Time.deltaTime;
 		}
 	if(Input.GetKey(KeyCode.D)){
-		myTransform.position+= Vector3.right*10*Time.deltaTime;
+		myTransform.position+= Vector3.right*cameraSpeed*Time.deltaTime;
 		}
 	
 	}

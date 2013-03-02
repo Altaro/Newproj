56022bc6ec1399bf9a8a0a5185f26365492bf9bf
Dimitry <AltaroX@gmail.com>
2013-03-02 13:55:15 +0200

    0.0.0.5a
    Полностю переработал систему движения. Добавил повороты.

--- Assets/Battle_Space/Ship scripts/enginesControl.cs
+++ Assets/Battle_Space/Ship scripts/enginesControl.cs
@@ -6,30 +6,39 @@ public class enginesControl : MonoBehaviour {
 	private float enginePower=10; //Временная переменная, потом этот параметр будет братся из БД уже когда статы кораблей будут разные.
 	private Transform myTransform; // Ну это понятно, трансформ как везде
 	protected bool moveStarted=false; //очень кривое и надеюсь временное решение для реализации плавности движения. 
-	protected Vector3 startPosition;
-	protected int distanceForMoved;
+	protected bool turnStartedLeft=false; 
+	protected bool turnStartedRight=false; 
 	
 	// Use this for initialization
 	void Start () {
 		myTransform=transform;
-		startPosition=myTransform.position;
 	}
 	
-	public void moveFront(int distance){
-		distanceForMoved=distance;
-		moveStarted=true;
+	public void moveFront(bool start){// Метод движения вперед
+		moveStarted=start;	
+	}
+	
+	public void turnLeft(bool start){//метод поворота
+		turnStartedLeft=start;		
+	}
+	
+	public void turnRight(bool start){//метод поворота
+		turnStartedRight=start;		
 	}
 	
 		
 	// Update is called once per frame
 	void Update () {
-	if(moveStarted){
-		if(Vector3.Distance(startPosition,myTransform.position)<distanceForMoved)	
+	if(moveStarted){	
 		myTransform.position+= myTransform.forward*enginePower*Time.deltaTime;
-			else{
-				startPosition=myTransform.position;
-				moveStarted=false;
-			}
+		}
+		
+		if(turnStartedLeft){
+		myTransform.Rotate(Vector3.up*Time.deltaTime*enginePower,Space.World);
+		}
+		
+		if(turnStartedRight){
+		myTransform.Rotate(Vector3.down*Time.deltaTime*enginePower,Space.World);
 		}
 		
 	}
--- Assets/Battle_Space/Ship scripts/testControllUnit.cs
+++ Assets/Battle_Space/Ship scripts/testControllUnit.cs
@@ -6,11 +6,12 @@ public class testControllUnit : MonoBehaviour {
 	
 	// Use this for initialization
 	void Start () {
-	GetComponent<enginesControl>().moveFront(50);
+	GetComponent<enginesControl>().moveFront(true);
+	GetComponent<enginesControl>().turnRight(true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
